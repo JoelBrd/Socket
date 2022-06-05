5e03e11483236c7ade6f09cefd5e5a80bfb18128
Joel Broad <38959456+Integern@users.noreply.github.com>
2022-06-05 11:39:45 +0100

    Have rodux store wrap around our top level component

--- src/Socket/Widget/RoactComponents/RoactMainWidget.lua
+++ src/Socket/Widget/RoactComponents/RoactMainWidget.lua
@@ -18,6 +18,8 @@ local RoactSearchBar ---@type RoactSearchBar
 local WidgetConstants ---@type WidgetConstants
 local SocketController ---@type SocketController
 local RoactBottomBar ---@type RoactBottomBar
+local RoactRodux ---@type RoactRodux
+local SocketConstants ---@type SocketConstants
 
 --------------------------------------------------
 -- Constants
@@ -25,53 +27,153 @@ local MARGIN_PADDING = UDim.new(0, 2)
 
 --------------------------------------------------
 -- Members
+local MainWidget ---@type RoactComponent
 
 ---
 ---@return RoactElement
 ---
 function RoactMainWidget:Get()
-    return Roact.createElement("Frame", {
-        BackgroundColor3 = WidgetConstants.Color.Background[SocketController:GetTheme()],
-        Size = UDim2.fromScale(1, 1),
-    }, {
-        UIPadding = Roact.createElement("UIPadding", {
-            PaddingBottom = UDim.new(0, 7),
-            PaddingLeft = MARGIN_PADDING,
-            PaddingRight = MARGIN_PADDING,
-            PaddingTop = MARGIN_PADDING,
-        }),
-        Container = Roact.createElement("Frame", {
-            BackgroundTransparency = 1,
+    return Roact.createElement(MainWidget)
+end
+
+---
+---@private
+---
+function RoactMainWidget:Create()
+    --------------------------------------------------
+    -- Create PlugContainer
+    MainWidget = Roact.Component:extend("MainWidget")
+
+    function MainWidget:render()
+        return Roact.createElement("Frame", {
+            BackgroundColor3 = WidgetConstants.Color.Background[SocketController:GetTheme()],
             Size = UDim2.fromScale(1, 1),
         }, {
-            UIListLayout = Roact.createElement("UIListLayout", {
-                Padding = MARGIN_PADDING,
-                FillDirection = Enum.FillDirection.Vertical,
-                SortOrder = Enum.SortOrder.LayoutOrder,
+            UIPadding = Roact.createElement("UIPadding", {
+                PaddingBottom = UDim.new(0, 7),
+                PaddingLeft = MARGIN_PADDING,
+                PaddingRight = MARGIN_PADDING,
+                PaddingTop = MARGIN_PADDING,
             }),
-            SearchHolder = Roact.createElement("Frame", {
+            Container = Roact.createElement("Frame", {
                 BackgroundTransparency = 1,
-                LayoutOrder = 1,
-                Size = UDim2.new(1, 0, 0, WidgetConstants.SearchBar.Pixel.LineHeight),
+                Size = UDim2.fromScale(1, 1),
             }, {
-                SearchContainer = RoactSearchBar:Get(),
+                UIListLayout = Roact.createElement("UIListLayout", {
+                    Padding = MARGIN_PADDING,
+                    FillDirection = Enum.FillDirection.Vertical,
+                    SortOrder = Enum.SortOrder.LayoutOrder,
+                }),
+                SearchHolder = Roact.createElement("Frame", {
+                    BackgroundTransparency = 1,
+                    LayoutOrder = 1,
+                    Size = UDim2.new(1, 0, 0, WidgetConstants.SearchBar.Pixel.LineHeight),
+                }, {
+                    SearchContainer = RoactSearchBar:Get(),
+                }),
+                PlugHolder = Roact.createElement("Frame", {
+                    BackgroundTransparency = 1,
+                    LayoutOrder = 2,
+                    Size = UDim2.new(1, 0, 1, -(WidgetConstants.SearchBar.Pixel.LineHeight + WidgetConstants.BottomBar.Pixel.LineHeight)),
+                }, {
+                    PlugContainer = RoactPlugContainer:Get(self.props),
+                }),
+                BottomHolder = Roact.createElement("Frame", {
+                    BackgroundTransparency = 1,
+                    LayoutOrder = 3,
+                    Size = UDim2.new(1, 0, 0, WidgetConstants.BottomBar.Pixel.LineHeight),
+                }, {
+                    BottomContainer = RoactBottomBar:Get(),
+                }),
             }),
-            PlugHolder = Roact.createElement("Frame", {
-                BackgroundTransparency = 1,
-                LayoutOrder = 2,
-                Size = UDim2.new(1, 0, 1, -(WidgetConstants.SearchBar.Pixel.LineHeight + WidgetConstants.BottomBar.Pixel.LineHeight)),
-            }, {
-                PlugContainer = RoactPlugContainer:Get(),
-            }),
-            BottomHolder = Roact.createElement("Frame", {
-                BackgroundTransparency = 1,
-                LayoutOrder = 3,
-                Size = UDim2.new(1, 0, 0, WidgetConstants.BottomBar.Pixel.LineHeight),
-            }, {
-                BottomContainer = RoactBottomBar:Get(),
-            }),
-        }),
-    })
+        })
+    end
+
+    --------------------------------------------------
+    -- Connect RoactRodux
+
+    ---@param state RoduxState
+    ---@param props table
+    local function mapStateToProps(state, props)
+        -- ODD FIX: Ignore duplicate call
+        if props.innerProps then
+            return
+        end
+
+        -- Construct groups
+        local groups = {} ---@type RoactPlugContainerProps.GroupInfo[]
+        local plugsState = state[SocketConstants.RoduxStoreKey.PLUGS]
+        local stateGroups = plugsState.Groups
+        for groupName, groupChild in pairs(stateGroups) do
+            -- Create group info
+            local groupInfo = {
+                name = groupName,
+                icon = nil,
+                isOpen = groupChild.UIState.IsOpen,
+                isVisible = true,
+                plugs = {}, ---@type RoactPlugContainerProps.GroupInfo[]
+            } ---@type RoactPlugContainerProps.GroupInfo
+            table.insert(groups, groupInfo)
+
+            -- Construct plugs
+            for plugModuleScript, plugChild in pairs(groupChild.Plugs) do
+                -- Create plug info
+                local plug = plugChild.Plug ---@type PlugDefinition
+                local plugInfo = {
+                    name = plug.Name,
+                    isOpen = plugChild.UIState.IsOpen,
+                    isVisible = true,
+                    plug = plug, ---@type PlugDefinition
+                    moduleScript = plugModuleScript,
+                } ---@type RoactPlugContainerProps.GroupInfo
+                table.insert(groupInfo.plugs, plugInfo)
+
+                -- Try populate group icon
+                if plug.GroupIcon and not groupInfo.icon then
+                    groupInfo.icon = plug.GroupIcon
+                end
+            end
+
+            -- Sort plugs
+            ---@param plugInfo0 RoactPlugContainerProps.PlugInfo
+            ---@param plugInfo1 RoactPlugContainerProps.PlugInfo
+            local function plugsSort(plugInfo0, plugInfo1)
+                return plugInfo0.name < plugInfo1.name
+            end
+            table.sort(groupInfo.plugs, plugsSort)
+        end
+
+        -- Sort groups
+        ---@param groupInfo0 RoactPlugContainerProps.GroupInfo
+        ---@param groupInfo1 RoactPlugContainerProps.GroupInfo
+        local function groupsSort(groupInfo0, groupInfo1)
+            return groupInfo0.name < groupInfo1.name
+        end
+        table.sort(groups, groupsSort)
+
+        -- Filter visibility from searchText
+        local searchText = plugsState.SearchText ---@type string
+        local searchTextUpper = searchText:upper()
+        if searchText:len() > 0 then
+            for _, groupInfo in pairs(groups) do
+                local groupHasGoodName = string.find(groupInfo.name:upper(), searchTextUpper) and true or false
+                local groupHasGoodPlug = false
+                for _, plugInfo in pairs(groupInfo.plugs) do
+                    local plugHasGoodName = string.find(plugInfo.name:upper(), searchTextUpper) and true or false
+                    plugInfo.isVisible = plugHasGoodName or groupHasGoodName
+                    groupHasGoodPlug = groupHasGoodPlug or plugHasGoodName
+                end
+                groupInfo.isVisible = groupHasGoodName or groupHasGoodPlug
+            end
+        end
+
+        -- Return props
+        return {
+            groups = groups,
+        }
+    end
+
+    MainWidget = RoactRodux.connect(mapStateToProps)(MainWidget)
 end
 
 ---
@@ -86,6 +188,8 @@ function RoactMainWidget:FrameworkInit()
     WidgetConstants = PluginFramework:Require("WidgetConstants")
     SocketController = PluginFramework:Require("SocketController")
     RoactBottomBar = PluginFramework:Require("RoactBottomBar")
+    SocketConstants = PluginFramework:Require("SocketConstants")
+    RoactRodux = PluginFramework:Require("RoactRodux")
 end
 
 ---
@@ -93,6 +197,8 @@ end
 ---
 ---Synchronously called, one after the other, with all other FrameworkStart()
 ---
-function RoactMainWidget:FrameworkStart() end
+function RoactMainWidget:FrameworkStart()
+    RoactMainWidget:Create()
+end
 
 return RoactMainWidget
--- src/Socket/Widget/RoactComponents/RoactPlugContainer.lua
+++ src/Socket/Widget/RoactComponents/RoactPlugContainer.lua
@@ -37,14 +37,6 @@ local WidgetConstants ---@type WidgetConstants
 
 --------------------------------------------------
 -- Members
-local PlugContainer ---@type RoactComponent
-
----
----@return RoactElement
----
-function RoactPlugContainer:Get()
-    return Roact.createElement(PlugContainer)
-end
 
 ---Creates all of the individual lines to populate the PlugContainer
 ---@param props table
@@ -121,119 +113,28 @@ local function createLinesFragment(props)
 end
 
 ---
----@private
+---@param props table
+---@return RoactElement
 ---
-function RoactPlugContainer:Create()
-    --------------------------------------------------
-    -- Create PlugContainer
-    PlugContainer = Roact.Component:extend("PlugContainer")
-
-    function PlugContainer:render()
-        return Roact.createElement("ScrollingFrame", {
-            BackgroundTransparency = 1,
-            Size = UDim2.fromScale(1, 1),
-            CanvasSize = UDim2.new(0, 0, 1, 0),
-            ScrollBarThickness = 6,
-        }, {
-            UIPadding = Roact.createElement("UIPadding", {
-                PaddingBottom = UDim.new(0, 4),
-                PaddingLeft = UDim.new(0, 4),
-                PaddingRight = UDim.new(0, 10),
-                PaddingTop = UDim.new(0, 4),
-            }),
-            UIListLayout = Roact.createElement("UIListLayout", {
-                FillDirection = Enum.FillDirection.Vertical,
-                SortOrder = Enum.SortOrder.LayoutOrder,
-            }),
-            Lines = createLinesFragment(self.props),
-        })
-    end
-
-    --------------------------------------------------
-    -- Connect RoactRodux
-
-    ---@param state RoduxState
-    ---@param props table
-    local function mapStateToProps(state, props)
-        -- ODD FIX: Ignore duplicate call
-        if props.innerProps then
-            return
-        end
-
-        -- Construct groups
-        local groups = {} ---@type RoactPlugContainerProps.GroupInfo[]
-        local plugsState = state[SocketConstants.RoduxStoreKey.PLUGS]
-        local stateGroups = plugsState.Groups
-        for groupName, groupChild in pairs(stateGroups) do
-            -- Create group info
-            local groupInfo = {
-                name = groupName,
-                icon = nil,
-                isOpen = groupChild.UIState.IsOpen,
-                isVisible = true,
-                plugs = {}, ---@type RoactPlugContainerProps.GroupInfo[]
-            } ---@type RoactPlugContainerProps.GroupInfo
-            table.insert(groups, groupInfo)
-
-            -- Construct plugs
-            for plugModuleScript, plugChild in pairs(groupChild.Plugs) do
-                -- Create plug info
-                local plug = plugChild.Plug ---@type PlugDefinition
-                local plugInfo = {
-                    name = plug.Name,
-                    isOpen = plugChild.UIState.IsOpen,
-                    isVisible = true,
-                    plug = plug, ---@type PlugDefinition
-                    moduleScript = plugModuleScript,
-                } ---@type RoactPlugContainerProps.GroupInfo
-                table.insert(groupInfo.plugs, plugInfo)
-
-                -- Try populate group icon
-                if plug.GroupIcon and not groupInfo.icon then
-                    groupInfo.icon = plug.GroupIcon
-                end
-            end
-
-            -- Sort plugs
-            ---@param plugInfo0 RoactPlugContainerProps.PlugInfo
-            ---@param plugInfo1 RoactPlugContainerProps.PlugInfo
-            local function plugsSort(plugInfo0, plugInfo1)
-                return plugInfo0.name < plugInfo1.name
-            end
-            table.sort(groupInfo.plugs, plugsSort)
-        end
-
-        -- Sort groups
-        ---@param groupInfo0 RoactPlugContainerProps.GroupInfo
-        ---@param groupInfo1 RoactPlugContainerProps.GroupInfo
-        local function groupsSort(groupInfo0, groupInfo1)
-            return groupInfo0.name < groupInfo1.name
-        end
-        table.sort(groups, groupsSort)
-
-        -- Filter visibility from searchText
-        local searchText = plugsState.SearchText ---@type string
-        local searchTextUpper = searchText:upper()
-        if searchText:len() > 0 then
-            for _, groupInfo in pairs(groups) do
-                local groupHasGoodName = string.find(groupInfo.name:upper(), searchTextUpper) and true or false
-                local groupHasGoodPlug = false
-                for _, plugInfo in pairs(groupInfo.plugs) do
-                    local plugHasGoodName = string.find(plugInfo.name:upper(), searchTextUpper) and true or false
-                    plugInfo.isVisible = plugHasGoodName or groupHasGoodName
-                    groupHasGoodPlug = groupHasGoodPlug or plugHasGoodName
-                end
-                groupInfo.isVisible = groupHasGoodName or groupHasGoodPlug
-            end
-        end
-
-        -- Return props
-        return {
-            groups = groups,
-        }
-    end
-
-    PlugContainer = RoactRodux.connect(mapStateToProps)(PlugContainer)
+function RoactPlugContainer:Get(props)
+    return Roact.createElement("ScrollingFrame", {
+        BackgroundTransparency = 1,
+        Size = UDim2.fromScale(1, 1),
+        CanvasSize = UDim2.new(0, 0, 1, 0),
+        ScrollBarThickness = 6,
+    }, {
+        UIPadding = Roact.createElement("UIPadding", {
+            PaddingBottom = UDim.new(0, 4),
+            PaddingLeft = UDim.new(0, 4),
+            PaddingRight = UDim.new(0, 10),
+            PaddingTop = UDim.new(0, 4),
+        }),
+        UIListLayout = Roact.createElement("UIListLayout", {
+            FillDirection = Enum.FillDirection.Vertical,
+            SortOrder = Enum.SortOrder.LayoutOrder,
+        }),
+        Lines = createLinesFragment(props),
+    })
 end
 
 ---
@@ -254,8 +155,6 @@ end
 ---
 ---Synchronously called, one after the other, with all other FrameworkStart()
 ---
-function RoactPlugContainer:FrameworkStart()
-    RoactPlugContainer:Create()
-end
+function RoactPlugContainer:FrameworkStart() end
 
 return RoactPlugContainer
