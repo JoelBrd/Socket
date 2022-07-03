c913343eb7fb4a3795a48ff101eb37b6c70741c8
Joel Broad <38959456+Integern@users.noreply.github.com>
2022-07-03 16:48:13 +0100

    simpler "Create A Plug"

--- src/Studio/CorePlugs/CreatePlug/PlugTemplate.lua
+++ src/Studio/CorePlugs/CreatePlug/PlugTemplate.lua
@@ -1,41 +1,29 @@
 ---
----Plug Template
+-- Plug
 ---
 
 --------------------------------------------------
 -- Dependencies
-local ServerStorage = game:GetService("ServerStorage") ---@type ServerStorage
+local ServerStorage = game:GetService("ServerStorage")
 local Utils = ServerStorage.SocketPlugin:FindFirstChild("Utils")
-local Logger = require(Utils.Logger) ---@type Logger
+local Logger = require(Utils.Logger)
 
 --------------------------------------------------
 -- Members
 
----@type PlugDefinition
 local plugDefinition = {
-    Group = "%s", ---@type string
-    GroupColor = nil, ---@type Color3
-    GroupIcon = nil, ---@type string
-    GroupIconColor = nil, ---@type Color3
-
     Name = "%s",
-    NameColor = nil, ---@type Color3
     Icon = "%s",
-    IconColor = nil, ---@type Color3
-
-    Description = "%s", ---@type string
-    EnableAutomaticUndo = false, ---@type boolean
-    AutoRun = false, ---@type boolean
-    Keybind = {}, ---@type Enum.KeyCode[]
-    Fields = {}, ---@type PlugField[]
-
-    Function = nil, ---@type fun(plug:PlugDefinition, plugin:Plugin)
+    Description = "%s",
 }
 
----@param plug PlugDefinition
----@param plugin Plugin
 plugDefinition.Function = function(plug, plugin)
-    Logger:PlugInfo(plug, ("Hello, from %s!"):format(plug.Name))
+    Logger:PlugInfo(plug, ("Hello %s!"):format(plug.Name))
+    --[[
+        ...
+        Your Logic Here
+        ...
+    ]]
 end
 
 return plugDefinition
--- src/Studio/CorePlugs/CreatePlug/init.lua
+++ src/Studio/CorePlugs/CreatePlug/init.lua
@@ -14,7 +14,7 @@ local Plugs = ServerStorage.SocketPlugin.Plugs
 
 --------------------------------------------------
 -- Constants
-local OPEN_ON_LINE = 29
+local OPEN_ON_LINE = 24
 
 --------------------------------------------------
 -- Members
@@ -30,14 +30,18 @@ local plugDefinition = {
         {
             Type = "string",
             Name = "Name",
-        },
-        {
-            Type = "string",
-            Name = "Group",
+            IsRequired = true,
         },
         {
             Type = "string",
             Name = "Icon",
+            IsRequired = true,
+        },
+    },
+    State = {
+        FieldValues = {
+            Name = "Plug",
+            Icon = "💡",
         },
     },
 }
@@ -46,9 +50,8 @@ local plugDefinition = {
 ---@param plugin Plugin
 plugDefinition.Function = function(plug, plugin)
     -- Get Fields
-    local name = plug:GetFieldValue("Name") or "No Name"
-    local group = plug:GetFieldValue("Group") or "No Group"
-    local icon = plug:GetFieldValue("Icon") or "🔌"
+    local name = plug:GetFieldValue("Name")
+    local icon = plug:GetFieldValue("Icon")
 
     -- Create variables
     local description = ("%s Description"):format(name)
@@ -56,7 +59,7 @@ plugDefinition.Function = function(plug, plugin)
     -- Create plug
     local newPlug = Instance.new("ModuleScript") ---@type ModuleScript
     newPlug.Name = name:gsub(" ", "") -- Remove whitespace
-    newPlug.Source = script.PlugTemplate.Source:format(group, name, icon, description)
+    newPlug.Source = script.PlugTemplate.Source:format(name, icon, description, "%s")
     newPlug.Parent = Plugs
 
     -- Open
