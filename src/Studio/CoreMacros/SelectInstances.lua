b86bee31c67902a9202a278e43a17c514d2623e1
Joel Broad <38959456+Integern@users.noreply.github.com>
2022-07-09 12:54:05 +0100

    make select instances more versatile

--- src/Studio/CoreMacros/SelectInstances.lua
+++ src/Studio/CoreMacros/SelectInstances.lua
@@ -23,8 +23,13 @@ local description =
     "Will select all instances that are descendants of the selected instance, that meet our parameters defined in our fields.\n"
 description = ("%s\n%s"):format(description, "Name: The .Name of an eligible instance")
 description = ("%s\n%s"):format(description, "ClassName: Instance is eligible if :IsA(className) == true")
-description = ("%s\n%s"):format(description, "Child Name: Instance is eligible if :FindFirstChild(childName, recurse) == true")
-description = ("%s\n%s"):format(description, "Recurse Children: Defines `recurse` boolean value used in :FindFirstChild()")
+description = ("%s\n%s"):format(description, "Child Name: Instance is eligible if :didFindFirstChild(childName, recurse) == true")
+description = ("%s\n%s"):format(description, "Recurse Children: Defines `recurse` boolean value for `Child Name`")
+description = ("%s\n%s"):format(
+    description,
+    "Exact Match: If true, instance is eligible if e.g., its .Name == `Name`. If false, instance is eligible if only part of the e.g., .Name matches `Name`"
+)
+description = ("%s\n%s"):format(description, "Case Sensitive: If false, all eligibility logic ignores lower case/upper case")
 description = ("%s\n%s"):format(description, "Max Amount: Will not select any instances more than this")
 
 ---@type MacroDefinition
@@ -52,29 +57,73 @@ local macroDefinition = {
             Type = "boolean",
         },
         {
+            Name = "Exact Match",
+            Type = "boolean",
+        },
+        {
+            Name = "Case Sensitive",
+            Type = "boolean",
+        },
+        {
             Name = "Max Amount",
             Type = "number",
         },
-    }, ---@type MacroField[]
+    },
+    State = {
+        FieldValues = {
+            ["Recurse Children"] = false,
+            ["Exact Match"] = true,
+            ["Case Sensitive"] = true,
+        },
+    },
 }
+
+---@param parent Instance
+---@param name string
+---@param recurseChildren boolean
+---@param exactMatch boolean
+---@param caseSensitive boolean
+---@return boolean
+local function didFindFirstChild(parent, name, recurseChildren, exactMatch, caseSensitive)
+    local options = recurseChildren and parent:GetDescendants() or parent:GetChildren()
+    for _, option in pairs(options) do
+        local optionName = caseSensitive and option.Name or option.Name:upper()
+        local doesMatch = exactMatch and optionName == name or string.find(optionName, name)
+        if doesMatch then
+            return true
+        end
+    end
+
+    return false
+end
 
 ---@param instances Instance[]
 ---@param name string|nil
 ---@param className string|nil
 ---@param childName string|nil
 ---@param recurseChildren boolean
+---@param exactMatch boolean
+---@param caseSensitive boolean
 ---@param maxAmount number|nil
 ---@return Instances[]
-local function search(instances, name, className, childName, recurseChildren, maxAmount)
+local function search(instances, name, className, childName, recurseChildren, exactMatch, caseSensitive, maxAmount)
     local results = {}
+
+    -- Transform data
+    name = name and (caseSensitive and name or name:upper())
+    childName = childName and (caseSensitive and childName or childName:upper())
 
     -- Loop instance descendants
     for _, instance in pairs(instances) do
         for _, descendant in pairs(instance:GetDescendants()) do
+            -- Transform data
+            local descendantName = caseSensitive and descendant.Name or descendant.Name:upper()
+
             -- Check if an eligible instance
-            local hasMatchingName = not name or descendant.Name == name
+            local hasMatchingName = not name or (exactMatch and descendantName == name or string.find(descendantName, name))
+            local hasMatchingChildOfName = not childName
+                or didFindFirstChild(descendant, childName, recurseChildren, exactMatch, caseSensitive)
             local hasMatchingClassName = not className or descendant:IsA(className)
-            local hasMatchingChildOfName = not childName or descendant:FindFirstChild(childName, recurseChildren)
             if hasMatchingName and hasMatchingClassName and hasMatchingChildOfName then
                 table.insert(results, descendant)
             end
@@ -104,10 +153,12 @@ macroDefinition.Function = function(macro, plugin)
     local className = macro:GetFieldValue("ClassName")
     local childName = macro:GetFieldValue("Child Name")
     local recurseChildren = macro:GetFieldValue("Recurse Children") and true or false
+    local exactMatch = macro:GetFieldValue("Exact Match") and true or false
+    local caseSensitive = macro:GetFieldValue("Case Sensitive") and true or false
     local maxAmount = macro:GetFieldValue("Max Amount")
 
     -- Search
-    local results = search(selectedInstances, name, className, childName, recurseChildren, maxAmount)
+    local results = search(selectedInstances, name, className, childName, recurseChildren, exactMatch, caseSensitive, maxAmount)
 
     -- Select
     Selection:Set(results)
