6120ca8860b62c3154a8a28a04a4ad497937e93d
Joel Broad <38959456+Integern@users.noreply.github.com>
2022-07-09 12:31:01 +0100

    cleaner api from prev. commit

--- src/Macros/MacroClientServer.lua
+++ src/Macros/MacroClientServer.lua
@@ -42,8 +42,7 @@ function MacroClientServer:RunTransfer()
     elseif IS_CLIENT then
         -- Wrap in pcall so "WaitForChild" infinite yield message doesn't appear in output
         local clonedDirectoryFolder = pcall(function()
-            return game.ReplicatedStorage:FindFirstChild(CLONED_DIRECTORY_FOLDER_NAME)
-                or game.ReplicatedStorage:WaitForChild(CLONED_DIRECTORY_FOLDER_NAME)
+            return game.ReplicatedStorage:WaitForChild(CLONED_DIRECTORY_FOLDER_NAME)
         end)
 
         -- Clear client
