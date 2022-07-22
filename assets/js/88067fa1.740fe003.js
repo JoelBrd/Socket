6f2c65c22a572e480b418ffc10859abd06d93bca
Joel Broad <38959456+Integern@users.noreply.github.com>
2022-07-22 18:35:52 +0100

    Built and published by Moonwave

---
+++ assets/js/88067fa1.740fe003.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[346],{97868:function(e){e.exports=JSON.parse('{"sourceUrl":"https://github.com/JoelBrd/Socket/blob/master","baseUrl":"/Socket/","classOrder":["MacroDefinition","MacroField","MacroFieldType","SocketSettings"],"apiCategories":[]}')}}]);
