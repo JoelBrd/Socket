d39c559a5fd510e6105f68ec8157755b06556f73
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-07-30 14:38:23 +0100

    Save/Load module scripts under our LocalMacros folder

--- src/Macros/LocalMacros.lua
+++ src/Macros/LocalMacros.lua
@@ -9,26 +9,59 @@ local LocalMacros = {}
 local PluginFramework = require(script:FindFirstAncestor("PluginFramework")) ---@type Framework
 local StudioHandler ---@type StudioHandler
 local StudioUtil ---@type StudioUtil
+local PluginHandler ---@type PluginHandler
+local PluginConstants ---@type PluginConstants
 
 ---
 ---Creates a folder for our local macros, and creates module scripts
 ---
 function LocalMacros:Load()
-    -- Create Directory
+    -- Unload if needed
     if LocalMacros:GetOurDirectory() then
         -- Was not previously unloaded..
         LocalMacros:Unload()
     end
 
+    -- Create directory
     local studioUserLocalMacrosFolder = Instance.new("Configuration")
     studioUserLocalMacrosFolder.Name = tostring(StudioUtil:GetUserIdentifier())
     studioUserLocalMacrosFolder.Parent = StudioHandler.Folders.LocalMacros
+
+    -- Read stored macros
+    local storedMacros = PluginHandler:GetSetting(PluginConstants.Setting.STORED_LOCAL_MACROS) or {}
+    for name, source in pairs(storedMacros) do
+        local moduleScript = Instance.new("ModuleScript") ---@type ModuleScript
+        moduleScript.Name = name
+        moduleScript.Source = source
+        moduleScript.Parent = studioUserLocalMacrosFolder
+    end
 end
 
 ---
 ---Unloads our LocalMacros by storing them in the plugin, and removing the directory
 ---
 function LocalMacros:Unload()
+    -- Write stored macros
+    local storedMacros = PluginHandler:GetSetting(PluginConstants.Setting.STORED_LOCAL_MACROS) or {}
+    for _, moduleScript in pairs(LocalMacros:GetOurDirectory():GetChildren()) do
+        -- WARN: Can only do singular modulescripts
+        if not moduleScript:IsA("ModuleScript") then
+            warn(("LocalMacro instance %q is not a `ModuleScript`, so was not saved."):format(moduleScript:GetFullName()))
+            moduleScript.Parent = StudioHandler.Folders.Directory
+        else
+            -- WARN: Children!
+            local children = moduleScript:GetChildren()
+            for _, child in pairs(children) do
+                warn(("LocalMacro instance %q has a child %q. This cannot be saved!"):format(moduleScript:GetFullName(), child.Name))
+            end
+
+            -- Store!
+            storedMacros[moduleScript.Name] = moduleScript.Source
+        end
+    end
+    PluginHandler:SetSetting(PluginConstants.Setting.STORED_LOCAL_MACROS, storedMacros)
+
+    -- Remove directory
     local directory = LocalMacros:GetOurDirectory()
     if directory then
         directory:Destroy()
@@ -46,6 +79,8 @@ end
 function LocalMacros:FrameworkInit()
     StudioUtil = PluginFramework:Require("StudioUtil")
     StudioHandler = PluginFramework:Require("StudioHandler")
+    PluginHandler = PluginFramework:Require("PluginHandler")
+    PluginConstants = PluginFramework:Require("PluginConstants")
 end
 
 ---@private
