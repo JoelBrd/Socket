52c80de6ce3aa62730b9b4bcb973f3bf30f63910
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-07-31 11:56:43 +0100

    Built and published by Moonwave

---
+++ assets/js/bb3fa2a6.00b87187.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[569],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/JoelBrd/Socket/blob/main","baseUrl":"/Socket/","classOrder":["MacroDefinition","MacroField","MacroFieldType","SocketSettings"],"apiCategories":[]}')}}]);
--- assets/js/bb3fa2a6.0ac62475.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[569],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/JoelBrd/Socket/blob/master","baseUrl":"/Socket/","classOrder":["MacroDefinition","MacroField","MacroFieldType","SocketSettings"],"apiCategories":[]}')}}]);
