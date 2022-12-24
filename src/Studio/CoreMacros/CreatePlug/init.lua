09d1c2940b843c10c90cb3f08eb0f6deefbc8622
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-12-24 14:16:01 +0000

    localmacros; bugfix with duplicate names, create macro "is local" field

--- src/Studio/CoreMacros/CreatePlug/init.lua
+++ src/Studio/CoreMacros/CreatePlug/init.lua
@@ -11,6 +11,8 @@
 local Selection = game:GetService("Selection") ---@type Selection
 local ServerStorage = game:GetService("ServerStorage") ---@type ServerStorage
 local Macros = ServerStorage.SocketPlugin.Macros
+local Utils = ServerStorage.SocketPlugin:FindFirstChild("Utils")
+local StudioUtil = require(Utils.StudioUtil)
 
 --------------------------------------------------
 -- Constants
@@ -18,6 +20,10 @@ local OPEN_ON_LINE = 24
 
 --------------------------------------------------
 -- Members
+
+local function getLocalMacrosDirectory()
+    return ServerStorage.SocketPlugin.LocalMacros:FindFirstChild(tostring(StudioUtil:GetUserIdentifier()))
+end
 
 ---@type MacroDefinition
 local macroDefinition = {
@@ -36,11 +42,17 @@ local macroDefinition = {
             Name = "Icon",
             IsRequired = true,
         },
+        {
+            Type = "boolean",
+            Name = "Is Local",
+            IsRequired = true,
+        },
     },
     State = {
         FieldValues = {
             Name = "Macro",
             Icon = "💡",
+            ["Is Local"] = false,
         },
     },
 }
@@ -51,6 +63,7 @@ macroDefinition.Function = function(macro, plugin)
     -- Get Fields
     local name = macro:GetFieldValue("Name")
     local icon = macro:GetFieldValue("Icon")
+    local isLocal = macro:GetFieldValue("Is Local")
 
     -- Create variables
     local description = ("%s Description"):format(name)
@@ -59,7 +72,7 @@ macroDefinition.Function = function(macro, plugin)
     local newMacro = Instance.new("ModuleScript") ---@type ModuleScript
     newMacro.Name = name:gsub(" ", "") -- Remove whitespace
     newMacro.Source = script.MacroTemplate.Source:format(name, icon, description, "%s")
-    newMacro.Parent = Macros
+    newMacro.Parent = isLocal and getLocalMacrosDirectory() or Macros
 
     -- Open
     plugin:OpenScript(newMacro, OPEN_ON_LINE)
